e0973e0802cc45ed9e3d703f7a7abed70af4f8fd
Dane Springmeyer <dane@mapbox.com>
2015-06-14 11:51:20 -0700

    catch remaining variable shadowing issues in IntPoint and DoublePoint

--- cpp/fix_members.sh
+++ cpp/fix_members.sh
@@ -6,15 +6,19 @@ perl -i -p -e "s/\.X/\.x/g;" ${ROOTDIR}/clipper.*
 perl -i -p -e "s/\->X/\->x/g;" ${ROOTDIR}/clipper.*
 perl -i -p -e "s/cInt X;/cInt x;/g;" ${ROOTDIR}/clipper.*
 perl -i -p -e "s/double X;/double x;/g;" ${ROOTDIR}/clipper.*
-perl -i -p -e "s/X\(x\)/x\(x\)/g;" ${ROOTDIR}/clipper.*
+perl -i -p -e "s/X\(x\)/x\(_x\)/g;" ${ROOTDIR}/clipper.*
 perl -i -p -e "s/X\(\(/x\(\(/g;" ${ROOTDIR}/clipper.*
+perl -i -p -e "s/double x = 0/double _x = 0/g;" ${ROOTDIR}/clipper.*
+perl -i -p -e "s/cInt x = 0/cInt _x = 0/g;" ${ROOTDIR}/clipper.*
 
 perl -i -p -e "s/\.Y/\.y/g;" ${ROOTDIR}/clipper.*
 perl -i -p -e "s/\->Y/\->y/g;" ${ROOTDIR}/clipper.*
 perl -i -p -e "s/cInt Y;/cInt y;/g;" ${ROOTDIR}/clipper.*
 perl -i -p -e "s/double Y;/double y;/g;" ${ROOTDIR}/clipper.*
-perl -i -p -e "s/Y\(y\)/y\(y\)/g;" ${ROOTDIR}/clipper.*
+perl -i -p -e "s/Y\(y\)/y\(_y\)/g;" ${ROOTDIR}/clipper.*
 perl -i -p -e "s/Y\(\(/y\(\(/g;" ${ROOTDIR}/clipper.*
 perl -i -p -e "s/cInt          Y;/cInt          y;/g;" ${ROOTDIR}/clipper.*
+perl -i -p -e "s/double y = 0/double _y = 0/g;" ${ROOTDIR}/clipper.*
+perl -i -p -e "s/cInt y = 0/cInt _y = 0/g;" ${ROOTDIR}/clipper.*
 
 
